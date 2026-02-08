a06a10f6289f9f7e1e9e8bbde42b7fa852efae6e
123sudipto <sudipto002gayen@gmail.com>
2026-02-08 18:57:04 +0530

    add User Model

---
+++ my-app/src/model/User.ts
@@ -0,0 +1,45 @@
+import mongoose, {Schema, Document} from "mongoose";
+// import { unique } from "next/dist/build/utils";
+
+export interface User extends Document {
+    name: string;
+    email: string;
+    password: string;
+    role: string;
+    verifyCode: string;
+    isVerified: boolean;
+}
+
+const UserSchema: Schema<User> = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"]
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'is invalid']
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"]
+    },
+    role: {
+        type: String,
+        default: "user"
+    },
+    verifyCode: {
+        type: String
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const User = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+
+export default User;
